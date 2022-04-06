80ed2b2d2b14be780b94423917e4ba22234dbbcc
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-06 20:36:58 +0300

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 import { routerReducer } from 'react-router-redux';
-import {
-  combineReducers, createStore, applyMiddleware, compose,
-} from 'redux';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import dragonReducer from '../reducers/dragon';
 import RocketReducer from '../reducers/rocket';
@@ -13,14 +11,6 @@ const rootReducer = combineReducers({
   routing: routerReducer,
 });
 
-const store = createStore(
-  rootReducer,
-  { dragons: [] },
-
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
