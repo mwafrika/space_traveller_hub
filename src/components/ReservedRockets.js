9c3cadb5908a055212ac6fcb60a5edd1f76f4f00
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-06 20:36:45 +0300

    Update ReservedRockets.js

--- src/components/ReservedRockets.js
+++ src/components/ReservedRockets.js
@@ -1,15 +1,14 @@
-import React from 'react';
 import { useSelector } from 'react-redux';
 import classes from './ReservedRockets.module.css';
 
 const ReservedRockets = () => {
-  const rockets = useSelector((state) => state.rockets);
-  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
+  // eslint-disable-next-line max-len
+  const rockets = useSelector((state) => state.rockets.list.filter((rocket) => rocket.reserved === true));
 
   return (
     <>
       <ul className={classes.reservedRockets}>
-        {reservedRockets.map((rocket) => (
+        {rockets.map((rocket) => (
           <li key={rocket.id}>{rocket.name}</li>
         ))}
       </ul>
