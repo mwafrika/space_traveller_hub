8812285de88eb84c2573cfecb1a4c0cac2147fad
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-06 20:36:48 +0300

    Update RocketList.js

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -1,23 +1,33 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { displayRockets } from '../redux/actions/rocket';
+import { useSelector } from 'react-redux';
 import NavBar from './NavBar';
 import Rocket from './Rocket';
 import classes from './RocketList.module.css';
 
 const RocketList = () => {
-  const dispatch = useDispatch();
   const rockets = useSelector((state) => state.rockets);
 
-  useEffect(() => {
-    dispatch(displayRockets());
-  }, []);
+  if (rockets.loading) {
+    return (
+      <>
+        <NavBar />
+        <span>...Loading</span>
+      </>
+    );
+  }
+  if (rockets.error) {
+    return (
+      <>
+        <NavBar />
+        <span>...There is an error!</span>
+      </>
+    );
+  }
 
   return (
     <>
       <NavBar />
       <div className={classes.rocketListContainer}>
-        {rockets.map((rocket) => (
+        {rockets.list.map((rocket) => (
           <Rocket
             key={rocket.id}
             id={rocket.id}
