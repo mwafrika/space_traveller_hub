f9f3302e1cda80f5b93bcbe343fbec1bc57274b3
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-04-06 11:19:00 +0300

    Rename store file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store/store';
+import store from './redux/store';
 import './assets/css/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,19 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-<<<<<<< HEAD:src/redux/store/index.js
-import missions from '../reducers/missions';
-
-const rootReducer = combineReducers({ missions });
-=======
+import missionReducer from '../reducers/missions';
 import dragonReducer from '../reducers/dragon';
 import RocketReducer from '../reducers/rocket';
 
 const rootReducer = combineReducers({
   dragons: dragonReducer,
   rockets: RocketReducer,
+  missions: missionReducer,
 });
->>>>>>> develop:src/redux/store/store.js
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
