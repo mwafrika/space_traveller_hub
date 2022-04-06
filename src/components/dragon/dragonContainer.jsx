3117e6ac05c559007fc9e0f7293746df8ea7a32b
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-04-06 23:19:33 +0300

    Fix unrelated histories

--- src/components/dragon/dragonContainer.jsx
+++ src/components/dragon/dragonContainer.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Header from '../header/NavBar';
 import Dragons from './dragons';
 
-const dragonContainer = () => (
-  <>
-    <Header />
-    <Dragons />
-  </>
-);
+const dragonContainer = () => {
+  const dragons = useSelector((state) => state.dragons);
+  return (
+    <>
+      <Header />
+      <Dragons dragons={dragons} />
+    </>
+  );
+};
 
 export default dragonContainer;
