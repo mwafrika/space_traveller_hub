ea11fb05032fe1bcba49e706b895951d9ca21a59
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-07 10:15:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,22 +8,24 @@ import Dragon from './components/dragon/dragonContainer';
 import Missions from './components/missions/missions';
 import RocketList from './components/rockets/RocketList';
 import Profile from './components/dragon/profileContainer';
+import { missionsRetrieved } from './redux/actions/missions';
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(displayDragons());
+    dispatch(missionsRetrieved());
   }, []);
 
   return (
     <Router>
       <Routes>
-        <Route path='/' index element={<RocketList />} />
-        <Route path='/dragons' element={<Dragon />} />
-        <Route path='/missions' element={<Missions />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/" index element={<RocketList />} />
+        <Route path="/dragons" element={<Dragon />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
