75534a2e65578d0c1db011f82baa55fe51bd7151
Mnemba Chambuya <karimnembac@gmail.com>
2022-04-07 11:20:10 +0300

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,26 +1,15 @@
 /* eslint-disable no-underscore-dangle */
-<<<<<<< HEAD
-import { combineReducers, createStore, applyMiddleware } from 'redux';
-=======
 // import { routerReducer } from 'react-router-redux';
-import {
-  combineReducers, createStore, applyMiddleware, compose,
-} from 'redux';
->>>>>>> de33129c0bf1d1c42a1ff4261f9c0a31fc42f63f
+import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import missions from '../reducers/missions';
 import dragonReducer from '../reducers/dragon';
 import RocketReducer from '../reducers/rocket';
+import missions from '../reducers/missions';
 
 const rootReducer = combineReducers({
   dragons: dragonReducer,
   rockets: RocketReducer,
-<<<<<<< HEAD
   missions,
-=======
-  missions: missionReducer,
-  // routing: routerReducer,
->>>>>>> de33129c0bf1d1c42a1ff4261f9c0a31fc42f63f
 });
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
