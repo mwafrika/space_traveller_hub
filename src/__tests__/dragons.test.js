b56734d237db0995e318b515947fb0e0c937da2d
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-04-09 02:33:04 +0300

    Fix testing issues

--- src/__tests__/dragons.test.js
+++ src/__tests__/dragons.test.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
+import { createRoot } from 'react-dom/client';
 import store from '../redux/store';
 import '@testing-library/jest-dom';
 import Dragon from '../components/dragon/dragon';
 
-describe('Test dragon page', () => {
-  it('should print dragons very well', () => {
+describe('Test dragons list', () => {
+  it('should return the dragons to passto the childelements', () => {
     const dragon = {
       id: 'dragon1',
       name: 'Dragon 1',
@@ -15,6 +16,17 @@ describe('Test dragon page', () => {
       type: 'Rocket',
       reserved: false,
     };
+    const reservedItems = [
+      {
+        id: dragon.id,
+        name: dragon.name,
+        reserved: dragon.reserved,
+      },
+    ];
+    const div = document.createElement('div');
+    createRoot(div).render(
+      <Dragon title='My Dragons' reservedItems={reservedItems} />,
+    );
     const tree = renderer.create(
       <Provider store={store}>
         <Dragon key={dragon.id} dragon={dragon} />
